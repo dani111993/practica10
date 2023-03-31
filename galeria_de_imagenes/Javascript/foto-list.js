cfe4e3108820cd1dc9b4c62f342744bc1829d50d
dani111993 <125510493+dani111993@users.noreply.github.com>
2023-03-30 22:43:27 -0600

    Add files via upload

---
+++ galeria_de_imagenes/Javascript/foto-list.js
@@ -0,0 +1,11 @@
+const PHOTO_LIST =[
+    'C:/DesarrolloAW/galeria_de_imagenes/images/conejo.jpg',
+    'C:/DesarrolloAW/galeria_de_imagenes/images/gato.jpg',
+    'C:/DesarrolloAW/galeria_de_imagenes/images/lobo.jpg',
+    'C:/DesarrolloAW/galeria_de_imagenes/images/mono.jpg',
+    'C:/DesarrolloAW/galeria_de_imagenes/images/panda.jpg',
+    'C:/DesarrolloAW/galeria_de_imagenes/images/perro.jpeg',
+    'C:/DesarrolloAW/galeria_de_imagenes/images/suricata.jpg',
+    'C:/DesarrolloAW/galeria_de_imagenes/images/tigre.jpg',
+    'C:/DesarrolloAW/galeria_de_imagenes/images/zorro.jpg'
+];
